41f9996ef5cbbbd7b8c79258e0a4c994cd946495
Monish Ramesha <70026302+monishramesha@users.noreply.github.com>
2024-03-20 22:37:58 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -14,7 +14,7 @@ const CONFIG = {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['monishramesha/OpenWorld-DeepFakeAttribution', 'monishramesha/Blockchain-based-P2P-File-Sharing-System','monishramesha/OS-Migration-with-Music-Player-based-on-Caching-Algorithm', 'monishramesha/Multithreaded-Online-Quiz-System']
       },
     },
     external: {
